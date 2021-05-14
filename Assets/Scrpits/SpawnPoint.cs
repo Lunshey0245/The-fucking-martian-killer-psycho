d00cea3e79aadf773edc697e533bf8836a2e3bd7
Makinolazz <franco.barranca@hotmail.com>
2021-05-13 23:11:12 -0300

    Spawn point and obstacle
    Created spawn point object, placed it on scene and created obstacle prefab

---
+++ Assets/Scrpits/SpawnPoint.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPoint : MonoBehaviour
+{
+    [SerializeField]
+    public List<GameObject> _objectsToSpawn;
+    [SerializeField]
+    public float _secondsBetweenSpawns;
+
+    public List<GameObject> ObjectsToSpawn 
+    { 
+        get { return _objectsToSpawn; } 
+        set { _objectsToSpawn = value; } 
+    }
+    public float SecondsBetweenSpawns 
+    {
+        get { return _secondsBetweenSpawns; }
+        set { _secondsBetweenSpawns = value; } 
+    }
+
+    private bool isReady = true;
+
+    private void Update()
+    {
+        if (isReady)
+        {
+            isReady = false;
+            StartCoroutine(GetObjects());
+        }
+    }
+
+    IEnumerator GetObjects()
+    {
+        foreach (var oneObject in _objectsToSpawn)
+        {
+            SpawnObject(oneObject);
+            yield return new WaitForSeconds(_secondsBetweenSpawns);
+        }
+
+        isReady = true;
+    }
+
+    public void SpawnObject(GameObject spawnMe)
+    {
+        Instantiate(spawnMe, this.transform.position, Quaternion.identity, this.transform);
+    }
+}
