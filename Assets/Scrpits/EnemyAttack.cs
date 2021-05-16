8b86e07b38ca5f86c18f960d7422385ed0922c75
Makinolazz <franco.barranca@hotmail.com>
2021-05-15 23:05:24 -0300

    Enemy Bullet and Enemy attack functionality

---
+++ Assets/Scrpits/EnemyAttack.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public GameObject bullet;
+    public Transform bulletSpawnPoint;
+
+    public bool isReadyToAttack = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isReadyToAttack)
+        {
+            isReadyToAttack = false;
+            StartCoroutine(WaitAndShoot());
+        }
+    }
+
+    IEnumerator WaitAndShoot()
+    {
+        yield return new WaitForSeconds(Random.Range(3, 8));
+        Instantiate(bullet, bulletSpawnPoint.position, Quaternion.identity);
+        isReadyToAttack = true;
+    }
+}
---
+++ Assets/Scrpits/EnemyBullet.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    public float speed;
+    private Vector3 LastTargetPosition;
+
+    private void Start()
+    {
+        LastTargetPosition = GameObject.FindGameObjectWithTag("Player").transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(LastTargetPosition * speed * Time.deltaTime);
+    }
+}
